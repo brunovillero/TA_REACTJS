c647218e88db57759497c1c93be8f755b40b9c9b
brunovillero <brunovillero66@gmail.com>
2024-09-23 18:58:23 -0300

    TA16 completada

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="About">
+        <h1>About</h1>
+        <a href="/">Home</a>
+        <a href="/contact">Contact</a>
+    </div>
+  )
+}
---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div className="Contact">
+        <h1>Contact</h1>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+    </div>
+  )
+}
